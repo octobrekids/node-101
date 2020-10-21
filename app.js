e7466d52ebc9673005019a9dcf5d370aad6cdb58
octobrekids <chnrd.k@gmail.com>
2020-10-22 01:16:03 +0700

    Using pipe instead

--- app.js
+++ app.js
@@ -7,12 +7,7 @@ const fs = require('fs');
 const myReadStream = fs.createReadStream('readme.txt', 'utf8');
 const myWriteStream = fs.createWriteStream('writeme.txt');
 
-// listen on 'data' event
-myReadStream.on('data', (chunk) => {
-    console.log('new chunk received: ');
-    console.log(chunk);
-    myWriteStream.write(chunk);
-})
-
+// no need to listen on event 'data' and handle chuck by yourself
+myReadStream.pipe(myWriteStream);
 
 
