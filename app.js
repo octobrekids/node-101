f51fe6296ff297abdc11e6e90362f6c39691f8f6
octobrekids <chnrd.k@gmail.com>
2020-10-22 01:56:12 +0700

    Write and Read file sync and async

--- app.js
+++ app.js
@@ -3,16 +3,28 @@ const http = require('http');
 // file system module
 const fs = require('fs');
 
-// readable stream
-const myReadStream = fs.createReadStream('readme.txt', 'utf8');
-const myWriteStream = fs.createWriteStream('writeme.txt');
+// // readable stream
+// const myReadStream = fs.createReadStream('readme.txt', 'utf8');
+// const myWriteStream = fs.createWriteStream('writeme.txt');
 
-// listen on 'data' event
-myReadStream.on('data', (chunk) => {
-    console.log('new chunk received: ');
-    console.log(chunk);
-    myWriteStream.write(chunk);
-})
+// // listen on 'data' event
+// myReadStream.on('data', (chunk) => {
+//     console.log('new chunk received: ');
+//     console.log(chunk);
+//     myWriteStream.write(chunk);
+// })
 
 
+// sync
+// const data = fs.readFileSync('text.txt', 'utf8');
+// fs.writeFileSync('write.txt', data);
+
+
+// async 
+fs.readFile('text.txt', 'utf8', (err,data) => {
+    fs.writeFile('writeFile.txt', data, (err,result) => {
+        if(err) console.log(err)
+    });
+});
+console.log('test');
 
